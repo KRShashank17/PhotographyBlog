e49063d6a992e6ad4b8fb19b23f0cef0ca4b4747
Shashank K R <krshashank1118@gmail.com>
2024-03-01 18:27:28 +0530

    Initial Configurations - APPWRITE

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import conf from './conf/conf.js'
+
+function App() {
+  console.log(conf.appwriteUrl);
+
+  return (
+    <>
+      <h1>
+        Photography Blog Website
+      </h1>
+    </>
+  )
+}
+
+export default App
